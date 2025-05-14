8b50718f394b5281b58617e1a20783f7c6a9cbe7
fenohasina17 <fenohasinarasoavarison@gmail.com>
2025-05-14 14:57:24 +0300

    feat:add pos component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PosComponent } from './components/pos/pos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pos/pos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pos',
+  standalone: false,
+  templateUrl: './pos.component.html',
+  styleUrl: './pos.component.css'
+})
+export class PosComponent {
+
+}
