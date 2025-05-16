fe5ef6e09a23d25ebe4e96ae63b5db1ebac4cd2a
fenohasina17 <fenohasinarasoavarison@gmail.com>
2025-05-16 14:01:23 +0300

    feat: add product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'http://127.0.0.1:8000/api/categories';
+
+  constructor(private http: HttpClient) { }
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
