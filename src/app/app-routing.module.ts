d49f89071ad8e1d226723f70220db19f6a95464d
fenohasina17 <fenohasinarasoavarison@gmail.com>
2025-05-16 11:59:57 +0300

    feat:add pos template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PosComponent } from './components/pos/pos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pos', component: PosComponent }
 ];
 
 @NgModule({
